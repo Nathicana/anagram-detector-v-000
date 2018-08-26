399be5dbbd18b4acf1e243ffb9568e67421bae1d
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-26 22:08:38 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,6 +3,8 @@ class Anagram
 
 attr_accessor :word
 
+hash = []
+
 def initialize(word)
   @word = word
 end
@@ -10,6 +12,7 @@ end
 def match(array)
   array.select do |element| (%w(array))
     if element.split("") == true
+      hash << array
       
 end
 end
