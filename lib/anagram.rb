b48ef9a561687a64c372aba4af165cb9d03907a4
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-26 22:18:26 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,9 @@ end
 
 def match(words)
 
-      @words_hash = words.each_with_object(Hash.new []) do |word, hash|
-        hash[word.split('').sort] += [word]
+  array.select do |element| (%w(array))
+    if element.split("") == true
+      hash << element
     end
 end
 
