1897690c653f809f02256b3e6823e659510b398f
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-26 22:15:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,10 @@ def initialize(word)
 end
 
 def match(words)
-  
+
       @words_hash = words.each_with_object(Hash.new []) do |word, hash|
         hash[word.split('').sort] += [word]
-      
+    end  
 end
 
 end
